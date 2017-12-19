55dc7555f9946f712a969c56e7bc6d3918c0c3ee
Oldnickel <collins.nyamao@strathmore.edu>
2017-12-19 11:16:25 +0300

    Commit with firebase DB, Validated Login and Logout 2

---
+++ app/src/main/java/ke/co/comsterhomes/www/firebase24/Prescript.java
@@ -0,0 +1,13 @@
+package ke.co.comsterhomes.www.firebase24;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Prescript extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prescript);
+    }
+}
